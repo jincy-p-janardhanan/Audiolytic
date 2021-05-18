5e94088d03407aec102aeac2c3e201e4ae8641ce
adithyakprabhu <adithyakprabhu@gmail.com>
2021-05-19 00:35:17 +0530

    upload files updated

--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'templates/file_upload.html', {
+        'form': form
+    })
